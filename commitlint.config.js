c11b0793c04ac6c89a0c85da59e6ec5747a47dcc
zhangzhengyang <1074385735@qq.com>
2025-06-10 17:45:48 +0800

    docs: 添加commitlint允许的类型注释

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,19 @@
 export default { extends: ['@commitlint/config-conventional'] }
+
+/**
+ *    "feat",
+        "fix",
+        "perf",
+        "style",
+        "docs",
+        "test",
+        "refactor",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+        "wip",
+        "workflow",
+        "types",
+        "release"
+ */
