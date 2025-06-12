ccbb72b657b916d0b6f29c06f8d6ba6d85af452c
zhangzhengyang <1074385735@qq.com>
2025-06-12 22:21:13 +0800

    feat: 新增底部页脚组件，支持动态标题配置和暗黑模式适配

---
+++ src/layout/components/lay-footer/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer
+    class="flex items-center justify-center w-full pb-2 text-sm text-[rgba(0,0,0,0.6)] dark:text-[rgba(220,220,242,0.8)]"
+  >
+    Copyright © 2020-present
+    <a class="hover:text-primary!" href="https://github.com/zhangzhengyang27" target="_blank">
+      &nbsp;{{ TITLE }}
+    </a>
+  </footer>
+</template>
+
+<script setup lang="ts">
+import { getConfig } from '@/config'
+
+const TITLE = getConfig('Title')
+</script>
